c573d10dedf2d930f55ca74ff9da29bec1869fb7
CarolHall-NCSU-CBE <95452316+CarolHall-NCSU-CBE@users.noreply.github.com>
2023-06-05 16:31:27 -0400

    Update submissionscript.sh

--- example/submissionscript.sh
+++ example/submissionscript.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-rm -f inputs/*
-rm -f results/run*
-rm -f parameters/*
-rm -f checks/*
-rm -f outputs/*
 
-./path_to_executive_file_DMDPRIME20/executive_file_DMDPRIME20
+path_to_executive_file_DMDPRIME20/executive_file_DMDPRIME20
 
 
 
