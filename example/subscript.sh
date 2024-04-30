ca5a460484a3334711f8bb288fc21eebf047a8e6
CarolHall-NCSU-CBE <95452316+CarolHall-NCSU-CBE@users.noreply.github.com>
2024-04-30 19:11:44 -0400

    new example

---
+++ example/subscript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir results
+mkdir outputs
+mkdir parameters
+mkdir inputs
+mkdir checks
+
+# Generate initial configuration for new simulation:
+../src_tempaddtoenergyfile/initconfig
+
+#Annealing:	
+for i in {1..9}
+do ../src_tempaddtoenergyfile/DMDPRIME20 < inputs/annealtemp_$i > outputs/out_annealtemp_$i
+done
+
+#DMD simulations
+for i in {1..300}
+do ../src_tempaddtoenergyfile/DMDPRIME20 < inputs/simtemp > outputs/out_simtemp_$i
+done
